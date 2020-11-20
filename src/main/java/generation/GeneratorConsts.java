6c08df4588a5f23f30959fa05a5fdf3f2d7e1372
Piotr Ruciński <piruci88@gmail.com>
2020-11-20 14:16:42 +0100

    It's about difference between -= and =- operators.

--- src/main/java/generation/GeneratorConsts.java
+++ src/main/java/generation/GeneratorConsts.java
@@ -3,15 +3,16 @@ package generation;
 public final class GeneratorConsts {
 
     // lodz latitude and longitude
-    public static final double LATITUDE_CENTER = 510.759445;
+    public static final double LATITUDE_CENTER = 51.0759445;
     public static final double LONGITUDE_CENTER = 19.457216;
 
     // lodz latitude and longitude rectangle boundary
     // |LATITUDE_C - geolocalization latitude| < LATITUDE_B
-    public static final double LATITUDE_BOUNDARY = 100.0;
-    public static final double LONGITUDE_BOUNDARY = 100.0;
+    public static final double LATITUDE_BOUNDARY = 1.0;
+    public static final double LONGITUDE_BOUNDARY = 1.0;
 
-    public static final long TIME_SPEED = 1;
+    // time propagation of tick in minutes
+    public static final long TIME_STEP = 1;
 
 
 }
