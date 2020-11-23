936d6bc7bc33ab8ce78d38f6797fcd4ceb8812d3
Piotr Ruciński <piruci88@gmail.com>
2020-11-23 10:17:17 +0100

    Corrected LATITUDE_CENTER

--- src/main/java/generation/GeneratorConsts.java
+++ src/main/java/generation/GeneratorConsts.java
@@ -3,7 +3,7 @@ package generation;
 public final class GeneratorConsts {
 
     // lodz latitude and longitude
-    public static final double LATITUDE_CENTER = 51.0759445;
+    public static final double LATITUDE_CENTER = 51.759445;
     public static final double LONGITUDE_CENTER = 19.457216;
 
     // lodz latitude and longitude rectangle boundary
