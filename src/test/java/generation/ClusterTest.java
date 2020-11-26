f185b3108c57cf94760043530a05d60783e388b5
goloneczka <48791689+goloneczka@users.noreply.github.com>
2020-11-26 12:39:55 +0100

    Clustering, refactor tests

--- src/test/java/generation/ClusterTest.java
+++ src/test/java/generation/ClusterTest.java
@@ -9,38 +9,53 @@ import org.apache.commons.math3.ml.clustering.CentroidCluster;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ClusterTest {
 
+    List<POI> pois = new ArrayList<>() {{
+        add(new POI("A1", "desc", new Geolocalization(49.14, 15.11), POIType.outdoor, "1"));
+        add(new POI("A2", "desc", new Geolocalization(46.14, 18.11), POIType.outdoor, "1"));
+        add(new POI("B1", "desc", new Geolocalization(29.14, 15.11), POIType.outdoor, "1"));
+        add(new POI("C1", "desc", new Geolocalization(66.14, 65.11), POIType.outdoor, "1"));
+        add(new POI("C2", "desc", new Geolocalization(61.14, 75.21), POIType.outdoor, "1"));
+        add(new POI("B2", "desc", new Geolocalization(22.14, 19.11), POIType.outdoor, "1"));
+        add(new POI("B3", "desc", new Geolocalization(29.14, 9.21), POIType.outdoor, "1"));
+        add(new POI("C3", "desc", new Geolocalization(55.14, 64.21), POIType.outdoor, "1"));
+        add(new POI("A1", "desc", new Geolocalization(44.14, 17.11), POIType.outdoor, "1"));
+    }};
+
     @Test
+    public void KMeanPOIForAll() {
+        //given:
+        int clusters = 3;
+        POICluster poiCluster = new POICluster(clusters, 75);
+
+        for (int i = 0; i < 100; i++) {
+            //when:
+            Collections.shuffle(pois);
+            List<POI> reducedPois = pois.subList(0, new Random().nextInt(pois.size() - clusters) + clusters);
+            List<CentroidCluster<POIWrapper>> poisWrapper = poiCluster.KMeanPOI(reducedPois);
+
+            //then:
+            Assert.assertEquals(clusters, poisWrapper.size());
+            Assert.assertEquals(2, poisWrapper.get(0).getCenter().getPoint().length);
+            Assert.assertEquals(reducedPois.size(),
+                    poisWrapper.stream().mapToInt(cluster -> cluster.getPoints().size()).sum());
+            Assert.assertTrue(poisWrapper.stream().allMatch(new HashSet<>()::add));
+
+        }
+    }
+
+    @Test(expected = org.apache.commons.math3.exception.NumberIsTooSmallException.class)
     public void KMeanPOI() {
         //given:
-        POICluster poiCluster = new POICluster(3, 75);
-
-        List<POI> pois = new ArrayList<>();
-        pois.add(new POI("A1", "desc", new Geolocalization(49.14, 15.11), POIType.outdoor, "1"));
-        pois.add(new POI("A2", "desc", new Geolocalization(46.14, 18.11), POIType.outdoor, "1"));
-        pois.add(new POI("B1", "desc", new Geolocalization(29.14, 15.11), POIType.outdoor, "1"));
-        pois.add(new POI("C1", "desc", new Geolocalization(66.14, 65.11), POIType.outdoor, "1"));
-        pois.add(new POI("C2", "desc", new Geolocalization(61.14, 75.21), POIType.outdoor, "1"));
-        pois.add(new POI("B2", "desc", new Geolocalization(22.14, 19.11), POIType.outdoor, "1"));
-        pois.add(new POI("B3", "desc", new Geolocalization(29.14, 9.21), POIType.outdoor, "1"));
-        pois.add(new POI("C3", "desc", new Geolocalization(55.14, 64.21), POIType.outdoor, "1"));
-        pois.add(new POI("A1", "desc", new Geolocalization(44.14, 17.11), POIType.outdoor, "1"));
-
-
-        //when:
-        List<CentroidCluster<POIWrapper>> poisWrapper = poiCluster.KMeanPOI(pois);
+        int clusters = 10;
+        POICluster poiCluster = new POICluster(clusters, 75);
 
         //then:
-        Assert.assertEquals(3, poisWrapper.size());
-        Assert.assertTrue( "w zaleznosci od losowych center moga pojawic sie dwa przypadki",
-                poisWrapper.get(0).getPoints().size() == 3 ||
-                        poisWrapper.get(0).getPoints().size() == 6);
-        Assert.assertEquals(2, poisWrapper.get(0).getCenter().getPoint().length);
-
-
+        Assert.assertTrue(pois.size() < clusters);
+        poiCluster.KMeanPOI(pois);
     }
 }
+
