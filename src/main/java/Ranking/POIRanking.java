e47d8c16a99996dab0ed569300036e684c4dc706
Hubert Kowalski <hkowalski1998@gmail.com>
2020-12-02 19:42:35 +0100

    wstępna wersja rankingu

---
+++ src/main/java/Ranking/POIRanking.java
@@ -0,0 +1,64 @@
+package Ranking;
+
+import com.sun.tools.jconsole.JConsoleContext;
+import model.POI;
+import model.Trace;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.util.*;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+
+
+
+@AllArgsConstructor
+@Data
+
+public class POIRanking {
+
+    //visitors count in POI ranking
+    public static HashMap<POI, Integer> generateVisitorsCountRanking(List<POI> POIs, List<Trace> traces){
+
+        HashMap<POI,Integer> visitorsCount = new HashMap<>();
+        for(Trace trace : traces) {
+            visitorsCount.put(trace.getPointOfInterest(), visitorsCount.getOrDefault(trace.getPointOfInterest(), 0) + 1);
+        }
+
+        return sortByValue(visitorsCount);
+    }
+
+    //time spent in POI ranking
+    public static  HashMap<POI, Integer> generateTimeSpentRanking(List<POI> POIs, List<Trace> traces){
+        HashMap<POI,Integer> totalTimeSpent = new HashMap<>();
+        for(Trace trace : traces) {
+            Duration timeSpent = Duration.between(trace.getEntryTime(), trace.getExitTime());
+            totalTimeSpent.put(trace.getPointOfInterest(), totalTimeSpent.getOrDefault(trace.getPointOfInterest(), 0) + (int) timeSpent.toMinutes());
+        }
+        return sortByValue(totalTimeSpent);
+    }
+
+
+
+    public static HashMap<POI, Integer> sortByValue(HashMap<POI, Integer> toBeSorted)
+    {
+        List<Map.Entry<POI, Integer> > list = new LinkedList<>(toBeSorted.entrySet());
+
+        Collections.sort(list, new Comparator<Map.Entry<POI, Integer>>() {
+            public int compare(Map.Entry<POI, Integer> o1,Map.Entry<POI, Integer> o2)
+            {
+                return (o2.getValue()).compareTo(o1.getValue());
+            }
+        });
+
+        HashMap<POI, Integer> temp = new LinkedHashMap<POI, Integer>();
+        for (Map.Entry<POI, Integer> aa : list) {
+            temp.put(aa.getKey(), aa.getValue());
+        }
+        return temp;
+    }
+}
---
+++ src/test/java/generation/RankingTest.java
@@ -0,0 +1,66 @@
+package generation;
+
+import Ranking.POIRanking;
+import model.POI;
+import model.Trace;
+import model.User;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class RankingTest {
+
+    private List<User> users;
+    private List<POI> pois;
+    private List<Trace> traces;
+    private LocalDateTime time;
+    private int timeStep; // min
+    private TraceGenerator traceGenerator;
+
+    @Before
+    public void setUp() throws Exception {
+        this.users = new LinkedList<>();
+        this.pois = new LinkedList<>();
+         this.traces = new LinkedList<>();
+         //gen Users
+        for (int i = 0; i < 10; i++) {
+            this.users.add(UserFactory.getInstance().generate());
+        }
+        //gen Pois
+        for (int i = 0; i < 10; i++) {
+            this.pois.add(POIFactory.getInstance().generate());
+        }
+
+        this.time = LocalDateTime.now();
+        this.timeStep = 3;
+        this.traceGenerator = new TraceGenerator(users, pois, time);
+        LocalDateTime currentTime = this.time;
+        //gen Traces
+        do {
+            currentTime = currentTime.plusMinutes(this.timeStep);
+            traces.addAll(this.traceGenerator.generateTraces(currentTime));
+        } while (traces.size() < 100);
+    }
+
+    @Test
+    public void rankingTest() {
+
+        HashMap<POI, Integer> visitorsRanking = POIRanking.generateVisitorsCountRanking(pois, traces);
+        HashMap<POI, Integer> timeRanking = POIRanking.generateTimeSpentRanking(pois, traces);
+
+
+        Assert.assertTrue(traces.size() != 0);
+        Assert.assertTrue(users.size() != 0);
+        for(POI poi : visitorsRanking.keySet()){
+           Assert.assertTrue(visitorsRanking.get(poi) > 0 );
+        }
+        for(POI poi : timeRanking.keySet()){
+            Assert.assertTrue(timeRanking.get(poi) > 0 );
+        }
+    }
+}
