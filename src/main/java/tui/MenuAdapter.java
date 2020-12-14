7d4bffc6365a4f5c04cd16da5c60c33116ecc646
Piotr Ruciński <piruci88@gmail.com>
2020-12-14 11:34:42 +0100

    Added comments

--- src/main/java/tui/MenuAdapter.java
+++ src/main/java/tui/MenuAdapter.java
@@ -1,11 +1,14 @@
 package tui;
 
 /**
- * In comparison to menu can have predefined input
+ * In comparison to menu can skip passing input thanks to predefined input
  */
 public abstract class MenuAdapter implements Menu {
     private String input;
 
+    /**
+     * @param input predefined input, if null acts like menu
+     */
     public MenuAdapter(String input) {
         this.input = input;
     }
